cb68e63f553d9a6ab0042351a229dc9e2abb1cd2
Coranna Howard <me@komiga.com>
2013-04-24 20:33:53 -0400

    Util: added optional() and tcheck().

--- src/Util.lua
+++ src/Util.lua
@@ -10,6 +10,39 @@ function init()
 	math.randomseed(os.time())
 end
 
+function ternary(cond, x, y)
+	return (cond)
+		and x
+		or  y
+end
+
+function optional(value, default)
+	return (nil~=value)
+		and value
+		or default
+end
+
+function tcheck(x, tc, opt)
+	opt=Util.optional(opt, false)
+	if "table"==type(tc) then
+		for _, t in pairs(tc) do
+			if not
+				(t==type(x)
+				or (opt and "nil"==type(x)))
+			then
+				assert(false)
+			end
+		end
+		return true
+	else
+		assert(
+			tc==type(x)
+			or (opt and "nil"==type(x))
+		)
+		return true
+	end
+end
+
 function random(x, y)
 	--return rng:value(x, y)
 	return math.random(x, y)
@@ -19,25 +52,14 @@ function choose_random(table)
 	return table[Util.random(1, #table)]
 end
 
-function ternary(cond, x, y)
-	return (cond)
-		and x
-		or  y
-end
-
 -- takes:
 --	(rgba, alpha_opt),
 --	(rgb, alpha), or
 --	(rgb) with alpha=255
 function set_color_table(rgb, alpha)
-	alpha=Util.ternary(
-		nil==alpha,
-		Util.ternary(
-			nil~=rgb[4],
-			rgb[4],
-			255
-		),
-		alpha
+	alpha=Util.optional(
+		alpha,
+		Util.optional(rgb[4], 255)
 	)
 	Gfx.setColor(rgb[1],rgb[2],rgb[3], alpha)
 end
